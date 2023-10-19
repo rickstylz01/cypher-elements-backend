fc94c26380d8cddc20883cffbf4b27ae701d2f0f
Ricardo Maya <rickmaya@gmail.com>
2023-10-18 19:44:30 -0500

    initial commit

---
+++ src/main/java/com/example/cebackend/CeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cebackend/CeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
