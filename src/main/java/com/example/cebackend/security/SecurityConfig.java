3eee8085307a26d47c599da3e71fb859ef8ff15b
Ricardo Maya <rickmaya@gmail.com>
2023-10-21 17:58:32 -0500

    add the passwordEncoder and authJwtRequestFilter methods in the SecurityConfig file

--- src/main/java/com/example/cebackend/security/SecurityConfig.java
+++ src/main/java/com/example/cebackend/security/SecurityConfig.java
@@ -6,11 +6,22 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public JwtRequestFilter authJwtRequestFilter() {
+    return new JwtRequestFilter();
+  }
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
