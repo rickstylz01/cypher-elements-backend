fef3ea40af8d03b25dcf83410f755c8e50f99e71
Ricardo Maya <rickmaya@gmail.com>
2023-10-22 00:24:12 -0500

    create a method in UserService to encode a password and use it in creatUeser

--- src/main/java/com/example/cebackend/service/UserService.java
+++ src/main/java/com/example/cebackend/service/UserService.java
@@ -2,25 +2,52 @@ package com.example.cebackend.service;
 
 import com.example.cebackend.exceptions.InformationExistException;
 import com.example.cebackend.models.User;
+import com.example.cebackend.models.request.LoginRequest;
 import com.example.cebackend.repository.UserRepository;
+import com.example.cebackend.security.JWTUtils;
+import com.example.cebackend.security.MyUserDetails;
 import lombok.Data;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Data
 public class UserService {
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final JWTUtils jwtUtils;
+  private final AuthenticationManager authenticationManager;
 
   public User createUser(User userObject) {
     if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
-      userObject.setPassword(passwordEncoder.encode((userObject.getPassword())));
+      String encodedPassword = encodePassword(userObject.getPassword());
+      userObject.setPassword(encodedPassword);
       userRepository.save(userObject);
-
       return userObject;
     } else {
       throw new InformationExistException("user email address: " + userObject.getEmailAddress() + ", already exists.");
+    }
+  }
+
+  public String encodePassword(String password) {
+    return passwordEncoder.encode(password);
+  }
+
+  public Optional<String> loginUser(LoginRequest loginRequest) {
+    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword());
+    try {
+      Authentication authentication = authenticationManager.authenticate(authenticationToken);
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+      MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+      return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+    } catch (Exception e) {
+      return Optional.empty();
     }
   }
 
--- src/test/java/com/example/cebackend/service/UserServiceTest.java
+++ src/test/java/com/example/cebackend/service/UserServiceTest.java
@@ -2,14 +2,28 @@ package com.example.cebackend.service;
 
 import com.example.cebackend.exceptions.InformationExistException;
 import com.example.cebackend.models.User;
+import com.example.cebackend.models.request.LoginRequest;
 import com.example.cebackend.repository.UserRepository;
+import com.example.cebackend.security.JWTUtils;
+import com.example.cebackend.security.MyUserDetails;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -21,26 +35,14 @@ public class UserServiceTest {
   @InjectMocks
   private UserService userService;
 
-  @Test
-  public void testCreatUserWhenEmailDoesNotExist() {
-    // Arrange
-    User user = new User();
-    user.setEmailAddress("test@example.com");
-    user.setPassword("password");
-    user.setUserName("testUser");
+  @Mock
+  private AuthenticationManager authenticationManager;
 
-    when(userRepository.existsByEmailAddress(user.getEmailAddress())).thenReturn(false);
-    when(userRepository.save(user)).thenReturn(user);
+  @Mock
+  private PasswordEncoder passwordEncoder;
 
-    // Act
-    User createdUser = userService.createUser(user);
-
-    // Assert
-    assertNotNull(createdUser);
-    assertEquals("test@example.com", createdUser.getEmailAddress());
-    assertEquals("password", createdUser.getPassword());
-    assertEquals("testUser", createdUser.getUserName());
-  }
+  @Mock
+  private JWTUtils jwtUtils;
 
   @Test(expected = InformationExistException.class)
   public void testCreateUserWhenEmailExists() {
@@ -57,6 +59,104 @@ public class UserServiceTest {
 
     // Assert
     // Expect Exception
+  }
+
+  @Test
+  public void testEncodePassword() {
+    // Arrange
+    String rawPassword = "myPassword";
+    String encodedPassword = "$2a$10$randomencodedpassword"; // mock encoded pw
+
+    // Mock the behavior of the password encoder
+    when(passwordEncoder.encode(rawPassword)).thenReturn(encodedPassword);
+
+    // Act
+    String result = userService.encodePassword(rawPassword);
+
+    // Assert
+    assertEquals(encodedPassword, result);
+  }
+
+  @Test
+  public void testCreatUserWhenEmailDoesNotExist() {
+    // Arrange
+    User user = new User();
+    user.setEmailAddress("test@example.com");
+    user.setPassword("password");
+
+    when(userRepository.existsByEmailAddress(user.getEmailAddress())).thenReturn(false);
+    when(passwordEncoder.encode(user.getPassword())).thenReturn("encodedPassword");
+
+    // Act
+    User createdUser = userService.createUser(user);
+
+    // Assert
+    assertNotNull(createdUser);
+    assertEquals("test@example.com", createdUser.getEmailAddress());
+    assertEquals("encodedPassword", createdUser.getPassword());
+  }
+
+  private String encodePassword(String password) {
+    return BCrypt.hashpw(password, BCrypt.gensalt());
+  }
+
+  @Test
+  public void testLoginUserWithValidCredentials() {
+    // Arrange
+    String emailAddress = "test@example.com";
+    String password = "password";
+
+    User user = new User();
+    user.setEmailAddress(emailAddress);
+    user.setPassword(password);
+
+    LoginRequest loginRequest = new LoginRequest();
+    loginRequest.setEmailAddress(user.getEmailAddress());
+    loginRequest.setPassword(user.getPassword());
+
+    // Mocking the user retrieval by the service
+    when(userRepository.findUserByEmailAddress(emailAddress)).thenReturn(user);
+    when(passwordEncoder.matches(password, user.getPassword())).thenReturn(true);
+
+    // Mocking authentication manager behavior
+    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(emailAddress, password);
+    Authentication authentication = new UsernamePasswordAuthenticationToken(new MyUserDetails(user), null);
+    when(authenticationManager.authenticate(authenticationToken)).thenReturn(authentication);
+
+    // Mock JWT token generation
+    when(jwtUtils.generateJwtToken(any(MyUserDetails.class))).thenReturn("validJwtToken");
+
+    // Act
+    Optional<String> jwtToken = userService.loginUser(loginRequest);
+
+    // Assert
+    assertTrue(jwtToken.isPresent());
+    assertEquals("validJwtToken", jwtToken.get());
+  }
+
+  @Test
+  public void testLoginUserWithInvalidCredentials() {
+    // Arrange
+    String emailAddress = "test@example.com";
+    String password = "wrongPassword";
+
+    User user = new User();
+    user.setEmailAddress(emailAddress);
+    user.setPassword("correctPassword"); // Set a different password in the user object
+
+    LoginRequest loginRequest = new LoginRequest();
+    loginRequest.setEmailAddress(emailAddress);
+    loginRequest.setPassword(password);
+
+    // Mock the user retrieval by the service
+    when(userRepository.findUserByEmailAddress(emailAddress)).thenReturn(user);
+    when(passwordEncoder.matches(password, user.getPassword())).thenReturn(false);
+
+    // Act
+    Optional<String> jwtToken = userService.loginUser(loginRequest);
+
+    // Assert
+    assertFalse(jwtToken.isPresent());
   }
 
   @Test
