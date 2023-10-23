e4c56b9c3ae44be2e8c6b285d5d1d5533ad0ffbd
Ricardo Maya <rickmaya@gmail.com>
2023-10-23 15:01:25 -0500

    create service test for getEventById method in EventService

--- src/main/java/com/example/cebackend/service/EventService.java
+++ src/main/java/com/example/cebackend/service/EventService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EventService {
@@ -18,6 +19,10 @@ public class EventService {
 
   public List<Event> getAllEvents() {
     return eventRepository.findAll();
+  }
+
+  public Optional<Event> getEventById(Long eventId) {
+    return eventRepository.findById(eventId);
   }
 }
 
--- src/test/java/com/example/cebackend/service/EventServiceTest.java
+++ src/test/java/com/example/cebackend/service/EventServiceTest.java
@@ -9,8 +9,9 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -46,5 +47,38 @@ public class EventServiceTest {
     assertEquals(event1.getName(), returnedEvents.get(0).getName());
     assertEquals(event2.getName(), returnedEvents.get(1).getName());
 
+  }
+
+  @Test
+  public void testGetEventById() {
+    // Arrange
+    Event event1 = new Event();
+    event1.setId(1L);
+    event1.setName("Event One");
+
+    Event event2 = new Event();
+    event2.setId(2L);
+    event2.setName("Event Two");
+
+    List<Event> events = List.of(event1, event2);
+
+    // Mock the behavior of eventRepository.findAll()
+    when(eventRepository.findById(1L)).thenReturn(Optional.of(event1));
+    when(eventRepository.findById(2L)).thenReturn(Optional.of(event2));
+    when(eventRepository.findById(3L)).thenReturn(Optional.empty());
+
+    // Act
+    Optional<Event> foundEvent1 = eventService.getEventById(1L);
+    Optional<Event> foundEvent2 = eventService.getEventById(2L);
+    Optional<Event> nonExistentEvent = eventService.getEventById(3L);
+
+    // Assert
+    assertTrue(foundEvent1.isPresent());
+    assertEquals("Event One", foundEvent1.get().getName());
+
+    assertTrue(foundEvent2.isPresent());
+    assertEquals("Event Two", foundEvent2.get().getName());
+
+    assertFalse(nonExistentEvent.isPresent());
   }
 }
