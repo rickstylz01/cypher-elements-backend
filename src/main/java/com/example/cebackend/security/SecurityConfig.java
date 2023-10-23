71f338d56b9919fb08d0d37ac7bc5c0ecff6a85b
Ricardo Maya <rickmaya@gmail.com>
2023-10-23 18:36:19 -0500

    add antMatchers to permit anyone to access all events enpoint

--- src/main/java/com/example/cebackend/security/SecurityConfig.java
+++ src/main/java/com/example/cebackend/security/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeRequests()
       .antMatchers("/auth/users/login/", "/auth/users/register/").permitAll()
+      .antMatchers("/api/events/").permitAll()
       .antMatchers("/h2-console/**").permitAll()
       .antMatchers("/auth/users/hello/").permitAll()
       .anyRequest().authenticated()
