77905aab933b44a4d0349cbfda8b369c54dae802
Ricardo Maya <rickmaya@gmail.com>
2023-10-23 12:31:49 -0500

    add controller methods to register and log in a user

--- src/main/java/com/example/cebackend/controllers/UserController.java
+++ src/main/java/com/example/cebackend/controllers/UserController.java
@@ -1,14 +1,16 @@
 package com.example.cebackend.controllers;
 
+import com.example.cebackend.models.User;
+import com.example.cebackend.models.request.LoginRequest;
+import com.example.cebackend.models.response.LoginResponse;
 import com.example.cebackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @RestController
@@ -27,5 +29,31 @@ public class UserController {
   public ResponseEntity<?> getHello() {
     message.put("message", "Hello");
     return new ResponseEntity<>(message, HttpStatus.OK);
+  }
+
+  @PostMapping(path = "/register/")
+  public ResponseEntity<?> createUser(@RequestBody User userObject) {
+    User newUser = userService.createUser(userObject);
+    if (newUser != null) {
+      message.put("message", "success");
+      message.put("data", newUser);
+      return new ResponseEntity<>(message, HttpStatus.CREATED);
+    } else {
+      message.put("message", "email already exists");
+      return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+  }
+
+  @PostMapping(path = "/login/")
+  public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+    Optional<String> jwtToken = userService.loginUser(loginRequest);
+    if(jwtToken.isPresent()){
+      logger.info("Authentication is good for user " + loginRequest.getEmailAddress());
+      return ResponseEntity.ok(new LoginResponse(jwtToken.get()));
+    }
+    else{
+      logger.warning("Authentication failed for user " + loginRequest.getEmailAddress());
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Authentication failed"));
+    }
   }
 }
