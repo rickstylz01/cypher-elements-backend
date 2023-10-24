31caca4f87df85cc8a2e4d10fd41dd7ce8a65d5c
Ricardo Maya <rickmaya@gmail.com>
2023-10-23 21:05:15 -0500

    create controller to update event given an existing eventId

--- src/main/java/com/example/cebackend/controllers/EventController.java
+++ src/main/java/com/example/cebackend/controllers/EventController.java
@@ -22,7 +22,7 @@ public class EventController {
     this.eventService = eventService;
   }
 
-  @PostMapping("/create/")
+  @PostMapping("/")
   public ResponseEntity<?> createEvent(@RequestBody Event event) {
     Event createdEvent = eventService.createEvent(event);
     message.put("message", "success");
@@ -53,5 +53,17 @@ public class EventController {
     }
   }
 
-
+  @PutMapping("/{eventId}/")
+  public ResponseEntity<?> updateEvent(@PathVariable Long eventId, @RequestBody Event updatedEvent) {
+    try {
+      Event event = eventService.updateEvent(eventId, updatedEvent);
+      message.put("message", "success");
+      message.put("data", event);
+      return new ResponseEntity<>(message, HttpStatus.OK);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    } catch (Exception e) {
+      return new ResponseEntity<>("Error updating event", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
